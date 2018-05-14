3c6048fc03e265f2743b097bb321e139c805d652
Vitaliy <vitaliy.saviour@gmail.com>
2018-05-14 15:53:51 +0300

    Настройка сборки проекта

--- gulpfile.js
+++ gulpfile.js
@@ -6,6 +6,12 @@ var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
 var server = require("browser-sync").create();
+var minifyCss = require("gulp-csso");
+var minifyImage = require("gulp-imagemin");
+var webp = require("gulp-webp");
+var rename = require("gulp-rename");
+var run = require("run-sequence");
+var del = require("del");
 
 gulp.task("style", function() {
   gulp.src("source/sass/style.scss")
@@ -14,13 +20,31 @@ gulp.task("style", function() {
     .pipe(postcss([
       autoprefixer()
     ]))
-    .pipe(gulp.dest("source/css"))
+    .pipe(gulp.dest("build/css"))
+    .pipe(minifyCss())
+    .pipe(rename("style.min.css"))
+    .pipe(gulp.dest("build/css"))
     .pipe(server.stream());
 });
 
-gulp.task("serve", ["style"], function() {
+gulp.task("images", function () {
+  return gulp.src("source/img/**/*.{png,jpg,svg}")
+    .pipe(minifyImage([
+      minifyImage.optipng({optimizationLevel: 3}),
+      minifyImage.jpegtran({progressive: true}),
+      ]))
+    .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("webp", function() {
+  return gulp.src("source/img/**/*.jpg")
+    .pipe(webp({quality: 90}))
+    .pipe(gulp.dest("build/img"));
+})
+
+gulp.task("serve", function() {
   server.init({
-    server: "source/",
+    server: "build/",
     notify: false,
     open: true,
     cors: true,
@@ -30,3 +54,23 @@ gulp.task("serve", ["style"], function() {
   gulp.watch("source/sass/**/*.{scss,sass}", ["style"]);
   gulp.watch("source/*.html").on("change", server.reload);
 });
+
+gulp.task("build", function(done) {
+  run("clean", "copy", "style", "webp", "images", done);
+});
+
+gulp.task("copy", function () {
+  return gulp.src([
+  "source/fonts/**/*.{woff,woff2}",
+  "source/img/**",
+  "source/js/**",
+  "source/*.html"
+  ], {
+  base: "source"
+  })
+  .pipe(gulp.dest("build"));
+ });
+
+ gulp.task("clean", function () {
+  return del("build");
+ });
